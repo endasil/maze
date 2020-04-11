6d4a85498fa479f7bd0d482f0ca7c82bc1d0d45a
Jonas <endasil@gmail.com>
2020-04-11 14:17:30 +0200

    update

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Player : MonoBehaviour
+{
+
+    public LayerMask clickableLayer;
+
+    private NavMeshAgent navAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo, 100, clickableLayer))
+            {
+                navAgent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
