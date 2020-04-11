f2596a7025221aaa802a58ec64381e71fa94aa36
Jonas <endasil@gmail.com>
2020-04-11 18:49:55 +0200

    projectiles work.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
 {
 
     public LayerMask clickableLayer;
-
+    public GameObject projectile;
     private NavMeshAgent navAgent;
     // Start is called before the first frame update
     void Start()
@@ -20,11 +20,22 @@ public class Player : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitInfo;
 
-            if (Physics.Raycast(ray, out hitInfo, 100, clickableLayer))
+            if (Physics.Raycast(ray, out var hitInfo, 100, clickableLayer))
             {
                 navAgent.SetDestination(hitInfo.point);
+            }
+        }
+
+        if (Input.GetMouseButtonUp(1) || Input.GetKey(KeyCode.Space))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out var hitInfo, 100))
+            {
+                Vector3 relativePos = hitInfo.point - transform.position;
+                relativePos.y = 0.0f;
+                Instantiate(projectile, new Vector3(transform.position.x, 1, transform.position.z), Quaternion.LookRotation(relativePos, Vector3.up));
             }
         }
     }
