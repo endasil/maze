87595e827a1eeb942259b3fa4205614ada34f752
Jonas <endasil@gmail.com>
2020-05-07 00:33:43 +0200

    more files

---
+++ Assets/AScripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+    
+    
+public class GameManager : Singleton<GameManager>
+{
+    
+    public TextMeshProUGUI statsText;
+
+    public int dummy = 20;
+    public Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        statsText.SetText($"HP: {player.hp} gold: {player.gold} Keys:  {player.Keys} ");
+    }
+}
